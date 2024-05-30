344001443d5b5dcc6984a6807e5ac3f93851bd6b
Jake <grafjb@mail.uc.edu>
2024-05-30 15:29:01 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double creditCardBalance = 5000.0;
+        double interestRate = 0.17;
+
+
+        double interestOneMonth = creditCardBalance * (interestRate / 12);
+
+        double interestTwoMonths = (creditCardBalance + interestOneMonth) * (interestRate / 12);
+
+        System.out.println("Credit card balance: $" + creditCardBalance);
+        System.out.println("Interest due after one month: $" + interestOneMonth);
+        System.out.println("Interest due after two months: $" + interestTwoMonths);
+    }
+}
